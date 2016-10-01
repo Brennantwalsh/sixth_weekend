3b3ca9c71b3624299c1379c027c4efd234c66ddc
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-10-01 15:44:32 -0500

    beginning of rspec

--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -1,6 +1,6 @@
 # Create a thesaurus program. To create it, you will create two classes, Thesaurus and Entry.
 # A Thesaurus will contain many Entries. An Entry contains three primary attributes:
-# word, synonymns, and antonyms. Your application should contain the following features:
+# word, synonyms, and antonyms. Your application should contain the following features:
 # 1. The ability to add new words to a Thesaurus.
 # 2. The ability to delete a word from a Thesaurus. 
 # 3. The ability to look up a word's synonyms.
@@ -12,3 +12,61 @@
 # and which belongs in the Entry class.
 #
 # And... test your functionality using RSpec!
+
+class Thesaurus
+  attr_accessor :log
+
+  def initialize
+    @log = []
+  end
+
+  def add_word(word)
+    @log << Entry.new(word)
+  end
+
+  def delete_word(word)
+    @log.each do |entry|
+      if entry.word == word
+        @log.delete(entry)
+      end
+    end 
+  end
+
+  def look_up_synonyms(word)
+    @log.each do |entry|
+      if entry.word == word
+        return entry.synonyms
+      end
+    end
+  end
+
+  def look_up_antonyms(word)
+    @log.each do |entry|
+      if entry.word == word
+        return entry.antonyms
+      end
+    end
+  end
+end
+
+class Entry
+  attr_reader :word, :synonyms, :antonyms
+
+  def initialize(word)
+    @word = word
+    @synonyms = []
+    @antonyms = []
+  end
+
+  def add_synonym(word)
+    @synonyms << word
+  end
+
+  def add_antonym(word)
+    @antonyms << word
+  end
+end
+
+
+
+
